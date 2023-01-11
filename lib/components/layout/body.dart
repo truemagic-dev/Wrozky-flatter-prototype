784149bb61de132e1fe370fe12ae1e95ae31d56b
truemagic-dev <truemagicexist@gmail.com>
2023-01-11 18:07:39 +0400

    Add initial placoholder layout

---
+++ lib/components/layout/body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MagicExistMainContent extends StatelessWidget {
+  const MagicExistMainContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Placeholder(),
+    );
+  }
+}
---
+++ lib/components/layout/navigation_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MagicExistNavBar extends StatelessWidget {
+  const MagicExistNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder(fallbackHeight: 50);
+  }
+}
